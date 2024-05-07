6630814ce923d8d75dfe439a470e088317ffd340
devbigeazi <adesholatajudeen1@gmail.com>
2024-05-07 10:42:25 +0100

    connected to database using webhooks

--- middleware.ts
+++ middleware.ts
@@ -7,15 +7,3 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const isProtectedRoute = createRouteMatcher(["/api/webhooks/clerk"]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next*).*)", "/", "/(api|trpc)(.*)"],
-// };
