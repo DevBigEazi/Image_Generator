d0ccc1dd5e50df684983784aa5b6fc18b5978b29
devbigeazi <adesholatajudeen1@gmail.com>
2024-05-07 07:26:26 +0100

    Implemented clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({});
+export default authMiddleware({
+    publicRoutes:["/api/webhooks/clerk"]
+});
 
 export const config = {
   matcher: ["/((?!_next/image|_next/static|favicon.ico).*)", "/"],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher(["/api/webhooks/clerk"]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next*).*)", "/", "/(api|trpc)(.*)"],
+// };
