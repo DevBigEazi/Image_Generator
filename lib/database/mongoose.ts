071b00110b33609d76a62758308cd142b997c17a
devbigeazi <adesholatajudeen1@gmail.com>
2024-05-07 08:51:03 +0100

    downgraded clerk version

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,16 +2,13 @@ import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-// Define interface for caching Mongoose connection and promise
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-// Initialize cached Mongoose connection object
 let cached: MongooseConnection = (global as any).mongoose;
 
-// If cached connection object doesn't exist, initialize it
 if (!cached) {
   cached = (global as any).mongoose = {
     conn: null,
@@ -20,21 +17,17 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-  // Check if Mongoose connection object is already cached, if so, return it for optimization
   if (cached.conn) return cached.conn;
 
-  // If MongoDB connection URL is not defined, throw an error
-  if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-  // If Mongoose promise is not cached, establish database connection and cache the promise
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "Image Generator", // Specify database name
-      bufferCommands: false, // Disable buffering commands
+      dbName: "image_generator",
+      bufferCommands: false,
     });
 
-  // Await Mongoose promise to resolve to Mongoose connection object and cache it
   cached.conn = await cached.promise;
 
   return cached.conn;
